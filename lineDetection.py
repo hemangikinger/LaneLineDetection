f6ce109bb158ccf6ed8028f3b67e1ba527054716
Hemangi <hemangikinger@gmail.com>
2020-09-15 12:18:38 +0530

    interested_region

--- lineDetection.py
+++ lineDetection.py
@@ -6,4 +6,11 @@ import matplotlib.image as mpimg
 from moviepy.editor import VideoFileClip
 import math
 import pandas
-
+def interested_region(img, vertices):
+    if len(img.shape) > 2: 
+        mask_color_ignore = (255,) * img.shape[2]
+    else:
+        mask_color_ignore = 255
+        
+    cv2.fillPoly(np.zeros_like(img), vertices, mask_color_ignore)
+    return cv2.bitwise_and(img, np.zeros_like(img))
