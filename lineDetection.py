6c2a4840612c39197b550adaaffa15e55c83e872
Hemangi <hemangikinger@gmail.com>
2020-09-15 12:19:16 +0530

    hough transform space

--- lineDetection.py
+++ lineDetection.py
@@ -14,3 +14,8 @@ def interested_region(img, vertices):
         
     cv2.fillPoly(np.zeros_like(img), vertices, mask_color_ignore)
     return cv2.bitwise_and(img, np.zeros_like(img))
+def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
+    lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
+    line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    lines_drawn(line_img,lines)
+    return line_img
